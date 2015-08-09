dedc11ea80e029aaefe7b8f9115c2ffa402c2322
Vetri <vairamut@usc.edu>
2015-08-09 01:47:36 -0700

    Assignment-11-IfElseTernary completed

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -15,6 +15,22 @@
 
     /* WORK HERE */
     
+    if (dollars==4) {
+        itemToReturn=@"get out of my store";
+    }
+    else if (dollars==5) {
+        itemToReturn=@"have some gum";
+    }
+    else if (dollars==6) {
+        itemToReturn=@"have an apple";
+    }
+    else if (dollars==1000) {
+        itemToReturn=@"have an Apple computer";
+    }
+    else if (dollars==1000000000) {
+        itemToReturn=@"have The Big Apple";
+    }
+   
     NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
     return itemToReturn;
 }
@@ -22,11 +38,12 @@
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
     /* WORK HERE */
 
-    NSUInteger cost = 24;
+ //   NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    NSUInteger cost = self.getsDiscount? 24*.75:24;
+//    if (self.getsDiscount) {
+//        cost *= .75;
+//    }
     
     return cost;
 }
