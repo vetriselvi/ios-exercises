400cc49b23a011186f857b3a237d55484e90e408
Vetri <vairamut@usc.edu>
2015-08-19 04:35:13 -0700

    Array exercise completed

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,65 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+   NSArray *xyz = [characterString componentsSeparatedByString:@";"];
+    
+/*
+    // Object Literals
+    // 1. Arrays
+    NSArray *abc = @[characterString]; // same as [NSArray arrayWithObjects:characterString, nil];
+    
+    // 2. Dictionaries
+    NSDictionary *sampleDIctionary = @{ @"key" : @"value",
+                                        @"Key2" : @"value2" };
+    
+    // 3. String
+    NSString *stringLiteral = @"This is a string literal";
+    
+    // 4. Number/Value
+    NSNumber *numberLiteral = @(123);
+ 
+ */
+    
+    // Google: "format specifiers in Objective-c"; %d, %lu
+//    NSString *sentence = @"The quick brown fox jumped over the lazy dog";
+//    NSLog(@"%@", [[sentence componentsSeparatedByString:@" "] componentsJoinedByString:@""]);
+//    
+//    NSArray *sentenceArray = [sentence componentsSeparatedByString:@" "];
+//    NSString *sentenceString = [sentenceArray componentsJoinedByString:@""];
+//    
+//    NSLog(@"This is a character string:%@", characterString);
+//    NSLog(@"%@", characterString);
+    
+
+    return xyz;
+
 }
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    return [characterArray componentsJoinedByString:@";"];
 }
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:nil ascending:YES selector:@selector(localizedCaseInsensitiveCompare:)];
+//    [characterArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+    NSArray *sortedArray = [characterArray sortedArrayUsingDescriptors:@[sortDescriptor]];
+
+//
+    return sortedArray;
+    NSLog(@"%@ character array",characterArray);
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
-    /* WORK HERE */
-    return NO;
+    NSMutableArray *mutableArray = [characterArray mutableCopy];
+    NSPredicate *containsWorf = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] 'worf'"];
+    [mutableArray filterUsingPredicate:containsWorf];
+    
+    BOOL answer = mutableArray == nil || [mutableArray count] == 0;
+    
+    return (!answer);
 }
 
 @end
